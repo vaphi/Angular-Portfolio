d365526163968c8322bb29e78d348cd483e5b0b3
Va <phi.va96@gmail.com>
2023-04-08 03:22:19 -0400

    Adeed deploy steps to main

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// build --prod --base-href "https://vaphi.github.io/profile/"
+// npx angular-cli-ghpages --dir=dist/portfolio
+
 if (environment.production) {
   enableProdMode();
 }
